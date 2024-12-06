a43d658caacdfe0294a13dc17ca008a5b84512ed
Jmancip <julianmancipe33@gmail.com>
2024-12-06 17:50:02 -0500

    se realizan correcciones del menu

--- src/menu/Inventario.java
+++ src/menu/Inventario.java
@@ -284,34 +284,30 @@ public class Inventario {
 
     // Categoria  opcion 6 del menu
     public static void categoria() {
-        // HashMap se utiliza para almacenar la info de la categoria (llave, valor)
         System.out.println("*****************************************************************************");
         System.out.println("**              Cantidad de productos por categoría                        **");
         System.out.println("*****************************************************************************");
         System.out.println("**   Calcular Cantidad de Productos:                                       **");
         System.out.println("**   Permite conocer la cantidad de productos por categoria                **");
-        System.out.println("**              esta es la cantidad de productos                           **");
-        System.out.println("*                                                                 *");
+        System.out.println("**          \uD83D\uDC49 esta es la cantidad de productos                           **");
+        System.out.println("**                                                                         **");
+        // HashMap se utiliza para almacenar la info de la categoria (llave, valor)
         Map<String, Integer> categoriaTotal = new HashMap<>();
         try (BufferedReader leerCantProd = new BufferedReader(new FileReader(archivoProductos))) {
             String linea;
             while ((linea = leerCantProd.readLine()) != null) {
-                String[] campos = linea.split(";");
+                String[] campos = linea.split(",");
                 String categoria = campos[2];
                 int cantidadDisponible = Integer.parseInt(campos[4]);
-                // el metodo getOrDefault se usa para dar un valor cuando las claves son ausentes
+                                 // Metodo getOrDefault  para dar un valor cuando las claves son ausentes
                 categoriaTotal.put(categoria, categoriaTotal.getOrDefault(categoria, 0) + cantidadDisponible);
-
             }
         } catch (IOException e) {
-            //muestra con exactitud en donde esta el error de codigo
             e.printStackTrace();
-
         }
         for (Map.Entry<String, Integer> entry : categoriaTotal.entrySet()) {
-            System.out.println("Categoria " + entry.getKey() + " Total productos por categoria: " + entry.getValue());
+            System.out.println("**      Categoria   '" + entry.getKey() + "'        Total producto por categoria: " + entry.getValue()+  "    **");
         }
-        System.out.println("*                                                                 *");
         System.out.println("*******************************************************************");
 
     }
